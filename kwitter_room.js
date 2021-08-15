c0499de30b2150a48bc18dc461bf9cc8dc835788
Meisha234 <76579924+Meisha234@users.noreply.github.com>
2021-08-15 10:30:49 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC0ECpPWm4DFg5VtxTulb-rVT8BUWIdrns",
+    authDomain: "let-s-chat-web-app-proje-c1a22.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-proje-c1a22-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-proje-c1a22",
+    storageBucket: "let-s-chat-web-app-proje-c1a22.appspot.com",
+    messagingSenderId: "537763990247",
+    appId: "1:537763990247:web:d9e9525ed09993e4c4f042",
+    measurementId: "G-YV1W168WW4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
